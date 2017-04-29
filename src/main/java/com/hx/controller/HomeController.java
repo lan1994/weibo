0f972cd7ea1029508a385df0342d080bc9f6fe10
lan1994 <774745012@qq.com>
2017-04-29 23:29:19 +0800

    修改界面布局

--- src/main/java/com/hx/controller/HomeController.java
+++ src/main/java/com/hx/controller/HomeController.java
@@ -15,7 +15,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import sun.jvm.hotspot.debugger.Page;
 
 import java.util.ArrayList;
 import java.util.List;
